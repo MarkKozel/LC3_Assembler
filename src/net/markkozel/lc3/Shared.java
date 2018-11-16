0d77f3aee86892a4a240725dd3fce1a99bc77757
Mark Kozel <github@markkozel.net>
2018-11-16 15:43:33 -0800

    Added Imm and Reg Address convert functs to Shared

--- src/net/markkozel/lc3/Shared.java
+++ src/net/markkozel/lc3/Shared.java
@@ -22,7 +22,7 @@ public class Shared {
 
 	public String baseFilename = "";
 	public String path = "";
-	
+
 	public final int IMM5_MAX = 15;
 	public final int IMM5_MIN = -16;
 
@@ -46,26 +46,104 @@ public class Shared {
 	}
 
 	public String repeat(String str, int times) {
-        return new String(new char[times]).replace("\0", str);
-    }
-	
+		return new String(new char[times]).replace("\0", str);
+	}
+
 	/**
 	 * Pads left of str with paddingChar finalLen times
-	 * @param str String to pad
-	 * @param finalLength final length of str when done
-	 * @param paddingChar character to use to pad
+	 * 
+	 * @param str
+	 *            String to pad
+	 * @param finalLength
+	 *            final length of str when done
+	 * @param paddingChar
+	 *            character to use to pad
 	 * @return
 	 */
 	public String padWithChar(String str, int finalLen, String paddingChar) {
 		String result = str;
 		int currLen = str.length();
-		
-		if(currLen < finalLen){
-			for(int x = 0; x < (finalLen = currLen); x++){
+
+		if (currLen < finalLen) {
+			for (int x = 0; x < (finalLen - currLen); x++) {
 				result = paddingChar + result;
 			}
 		}
 		return result;
 	}
-	
+
+	/**
+	 * Converts a string Register Name (R0, R6) to 3-bit address
+	 * 
+	 * @param regName
+	 *            Register Name
+	 * @return 3-bit binary address of Register or 'null' if regName is bad
+	 */
+	public String regToAddress(String regName) {
+		String result = "";
+		int regNumber;
+
+		if (regName.length() != 2) {//Invalid Reg Name
+			return null;
+		}
+
+		if (!regName.toUpperCase().startsWith("R")) { //Doesn't start with r/R
+			return null;
+		}
+
+		try {
+			regNumber = Integer.parseInt(regName.substring(1));
+		} catch (NumberFormatException e) { //Not a number
+			return null;
+		}
+
+		if (regNumber > 7) {//R8 and R9 are invalid reg numbers
+			return null;
+		}
+
+		//OK, got good Reg Name and Number. Convert to 3-bit address and return as string
+		result = padWithChar(Integer.toBinaryString(regNumber), 3, "0");
+
+		return result;
+	}
+
+	/**
+	 * Converts Dec or Hex Immediate Value to bit string
+	 * 
+	 * @param immName
+	 *            Immediate value
+	 * @param numBits
+	 *            Number of bits in final result
+	 * @return bit string, numBits long (zero extended), of immediate value
+	 */
+	public String immToAddress(String immName, int numBits) {
+		String result = "";
+		int immValue;
+
+		if (!(immName.toUpperCase().startsWith("#") || immName.toUpperCase().startsWith("X"))) {//bad imm tag (#/x)
+			return null;
+		}
+
+		if (immName.toUpperCase().startsWith("#")) { //Decimal Value
+			try { //Make sure value is number
+				immValue = Integer.parseInt(immName.substring(1));
+			} catch (NumberFormatException e) { //Not a number
+				return null;
+			}
+			result = padWithChar(Integer.toBinaryString(immValue), numBits, "0");
+		}
+
+		if (immName.toUpperCase().startsWith("X")) { //Decimal Value
+			try { //Make sure value is number
+				immValue = Integer.parseInt(immName.substring(1), 16);
+			} catch (NumberFormatException e) { //Not a number
+				return null;
+			}
+
+			result = padWithChar(Integer.toBinaryString(immValue), numBits, "0");
+		}
+
+		return result;
+	}
+
 }
---
+++ src/net/markkozel/lc3/testShared.java
@@ -0,0 +1,127 @@
+package net.markkozel.lc3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class testShared {
+	public Shared shared;
+
+	@Test
+	public void initTest() {
+		shared = Shared.getInstance();
+		assertNotNull(shared);
+	}
+
+	@Test
+	public void updateFileInfoTest() {
+		shared = Shared.getInstance();
+		shared.updateFileInfo("c://MyDir//MyFile.jpeg");
+
+		assertEquals(shared.baseFilename, "MyFile");
+		assertEquals(shared.path, "c:\\MyDir");
+	}
+
+	@Test
+	public void repeatTest() {
+		shared = Shared.getInstance();
+
+		String test = "hello";
+		test = shared.repeat(test, 2);
+		assertEquals(test, "hellohello");
+	}
+
+	@Test
+	public void padWithCharTest() {
+		shared = Shared.getInstance();
+
+		//Append with 5 '0' chars
+		String test = "hello";
+		test = shared.padWithChar(test, 10, "0");
+		assertEquals(test, "00000hello");
+
+		//Append with no additional '0' chars
+		test = "hello";
+		test = shared.padWithChar(test, 5, "0");
+		assertEquals(test, "hello");
+
+		//finalSize is < original string size. Expect no chnage
+		test = "hello";
+		test = shared.padWithChar(test, 4, "0");
+		assertEquals(test, "hello");
+	}
+
+	@Test
+	public void regToAddressTest() {
+		shared = Shared.getInstance();
+
+		//RegName too short
+		String test = shared.regToAddress("s");
+		assertNull(test);
+
+		//RegName too long
+		test = shared.regToAddress("s23");
+		assertNull(test);
+
+		//RegName does not start with 'R'
+		test = shared.regToAddress("S1");
+		assertNull(test);
+
+		//RegName does not start with 'R'
+		test = shared.regToAddress("RR");
+		assertNull(test);
+
+		//RegName does not start with 'R'
+		test = shared.regToAddress("R8");
+		assertNull(test);
+		test = shared.regToAddress("R8");
+		assertNull(test);
+
+		//Good Reg Names
+		test = shared.regToAddress("R0");
+		assertEquals(test, "000");
+		test = shared.regToAddress("R1");
+		assertEquals(test, "001");
+		test = shared.regToAddress("R2");
+		assertEquals(test, "010");
+		test = shared.regToAddress("R3");
+		assertEquals(test, "011");
+		test = shared.regToAddress("R4");
+		assertEquals(test, "100");
+		test = shared.regToAddress("R5");
+		assertEquals(test, "101");
+		test = shared.regToAddress("R6");
+		assertEquals(test, "110");
+		test = shared.regToAddress("R7");
+		assertEquals(test, "111");
+	}
+
+	@Test
+	public void immToAddressTest() {
+		shared = Shared.getInstance();
+
+		//immName bad first char
+		String test = shared.immToAddress("{", 0);
+		assertNull(test);
+
+		//Decimal value not a number
+		test = shared.immToAddress("#GHG", 0);
+		assertNull(test);
+
+		//Decimal value is good
+		test = shared.immToAddress("#5", 5);
+		assertEquals(test, "00101");
+		
+		test = shared.immToAddress("x5", 5);
+		assertEquals(test, "00101");
+		
+		test = shared.immToAddress("xff", 8);
+		assertEquals(test, "11111111");
+		
+		test = shared.immToAddress("xff", 10);
+		assertEquals(test, "0011111111");
+		
+		test = shared.immToAddress("x00", 10);
+		assertEquals(test, "0000000000");
+	}
+}
